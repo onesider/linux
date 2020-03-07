0c14ae14fd635d2dc5674dea8aa4646489d97c68
onesider <onesider@gmail.com>
2020-03-08 07:23:14 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -52,8 +52,13 @@ mv .zshrc ~/.zshrc
 wget https://github.com/knqyf263/pet/releases/download/v0.3.0/pet_0.3.0_linux_amd64.deb
 dpkg -i pet_0.3.0_linux_amd64.deb
 
+#64bit
 wget https://github.com/Peltoche/lsd/releases/download/0.16.0/lsd-musl_0.16.0_amd64.deb
 dpkg -i lsd-musl_0.16.0_amd64.deb
+
+#32bit
+#wget https://github.com/Peltoche/lsd/releases/download/0.16.0/lsd-musl_0.16.0_i386.deb
+#dpkg -i lsd-musl_0.16.0_i386.deb
 
 wget https://github.com/sharkdp/bat/releases/download/v0.12.1/bat_0.12.1_amd64.deb
 dpkg -i bat_0.12.1_amd64.deb
