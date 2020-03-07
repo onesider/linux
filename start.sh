8f215de5ab9034a0f011dc6c5f3bf74a14ba9b46
onesider <onesider@gmail.com>
2020-03-08 02:25:07 +0900

    zplug 사용하기로함.

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 onesider="/home/onesider"
 
 echo "** apt setting!!"
+add-apt-repository ppa:x4121/ripgrep
+
 apt -y update
 apt -y upgrade
 apt -y dist-upgrade
@@ -19,6 +21,10 @@ apt install -y gcc
 apt install -y gdb
 apt install -y cscope
 apt install -y ctags
+apt install -y tig
+apt install -y ripgrep
+apt install -y jq
+apt install -y neofetch
 
 (echo "Y")|sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
@@ -42,13 +48,26 @@ mv ./linux/.vimrc ./.vimrc
 mv ./linux/.zshrc ./.zshrc
 rm -rf linux
 
-#echo "** set zsh !!"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone git://github.com/joelthelion/autojump.git
+#Util
+wget https://github.com/knqyf263/pet/releases/download/v0.3.0/pet_0.3.0_linux_amd64.deb
+dpkg -i pet_0.3.0_linux_amd64.deb
+
+wget https://github.com/Peltoche/lsd/releases/download/0.16.0/lsd-musl_0.16.0_amd64.deb
+dpkg -i lsd-musl_0.16.0_amd64.deb
+
+wget https://github.com/sharkdp/bat/releases/download/v0.12.1/bat_0.12.1_amd64.deb
+dpkg -i bat_0.12.1_amd64.deb
+
+wget https://github.com/sharkdp/fd/releases/download/v7.4.0/fd_7.4.0_amd64.deb
+dpkg -i fd_7.4.0_amd64.deb
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+rm -rf *.deb
 
 #이유는 모르겠는데 install.py가 잘 안되는 경우가 발생할수도 있다. 셀프로 설치해주면 된다.
-python3 ./autojump/install.py 
+python3 ./autojump/install.py
 
 #echo "** set font !!"
 git clone https://github.com/powerline/fonts.git
@@ -57,7 +76,7 @@ rm -rf fonts
 
 #path 편집
 if [ "${HOME}" == "${onesider}" ]; then
-	chown onesider:onesider -R .vim/ .oh-my-zsh/ autojump/ 
+	chown onesider:onesider -R .vim/ .oh-my-zsh/ autojump/
 	echo "finished! - onesider"
 else
 	sed -i "s|${onesider}|${HOME}|g" ${HOME}/.zshrc
